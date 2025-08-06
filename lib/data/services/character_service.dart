40276de93d78c2b69236cb370923eb7bacd7aeb1
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2025-08-06 19:39:27 -0300

    added char service and added get single char req

--- lib/data/services/character_service.dart
+++ lib/data/services/character_service.dart
@@ -1,17 +1,34 @@
+import 'dart:convert';
+
 import 'package:rick_and_morty_wiki/data/services/service.dart';
 import 'package:rick_and_morty_wiki/domain/models/charater.dart';
+import 'package:http/http.dart' as http;
+import 'package:rick_and_morty_wiki/exceptions/api_exception.dart';
+import 'package:rick_and_morty_wiki/utils/environment.dart';
 
 class CharacterService implements Service<Character> {
   @override
   Future<List<Character>> filter() {
-    // TODO: implement filter
+    // TODO: implement getASingle
     throw UnimplementedError();
   }
 
   @override
-  Future<Character> getASingle(int id) {
-    // TODO: implement getASingle
-    throw UnimplementedError();
+  Future<Character> getASingle(int id) async {
+    try {
+      final response = await http.get(
+        Uri.parse(
+          '${Environment.baseURL}/character/$id',
+        ),
+      );
+
+      final data = jsonDecode(response.body);
+      final character = Character.fromJson(data);
+
+      return character;
+    } catch (_) {
+      throw ApiException('Falha ao obter personagem');
+    }
   }
 
   @override
@@ -21,7 +38,7 @@ class CharacterService implements Service<Character> {
   }
 
   @override
-  Future<List<Character>> getMultiple() {
+  Future<List<Character>> getMultiple(String ids) {
     // TODO: implement getMultiple
     throw UnimplementedError();
   }
