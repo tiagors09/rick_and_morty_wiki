535bda383ce9ef69ac88492d558b20bd7bbf3167
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2025-08-06 19:15:03 -0300

    added location service

---
+++ lib/data/services/location_service.dart
@@ -0,0 +1,28 @@
+import 'package:rick_and_morty_wiki/data/services/service.dart';
+import 'package:rick_and_morty_wiki/domain/models/location.dart';
+
+class LocationService implements Service<Location> {
+  @override
+  Future<List<Location>> filter() {
+    // TODO: implement filter
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Location> getASingle(int id) {
+    // TODO: implement getASingle
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Location>> getAll() {
+    // TODO: implement getAll
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Location>> getMultiple() {
+    // TODO: implement getMultiple
+    throw UnimplementedError();
+  }
+}
