009a0aa9835a120f6ed11c7be641ef978a733e41
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2025-08-06 20:17:34 -0300

    refactored Service interface

--- lib/data/services/service.dart
+++ lib/data/services/service.dart
@@ -1,6 +1,3 @@
 abstract class Service<T> {
-  Future<List<T>> getAll();
-  Future<T> getASingle(int id);
-  Future<List<T>> getMultiple(String ids);
-  Future<List<T>> filter();
+  Future<T> execute();
 }
