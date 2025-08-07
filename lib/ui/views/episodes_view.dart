3ae81e4886a7f26d16023ae0fd3a179161946281
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2025-08-07 15:32:10 -0300

    added episodes view

---
+++ lib/ui/views/episodes_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class EpisodesView extends StatefulWidget {
+  const EpisodesView({super.key});
+
+  @override
+  State<EpisodesView> createState() => _EpisodesViewState();
+}
+
+class _EpisodesViewState extends State<EpisodesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Placeholder(),
+    );
+  }
+}
