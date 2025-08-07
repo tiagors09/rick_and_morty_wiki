10e1a58ea157a577d6df984785af62e47118495e
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2025-08-07 15:30:43 -0300

    added routes

--- lib/rick_and_morty_app.dart
+++ lib/rick_and_morty_app.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:rick_and_morty_wiki/ui/views/characters_view.dart';
+import 'package:rick_and_morty_wiki/ui/views/episodes_view.dart';
+import 'package:rick_and_morty_wiki/ui/views/locations_view.dart';
+import 'package:rick_and_morty_wiki/ui/views/selection_menu_view.dart';
 
 class RickAndMortyApp extends StatelessWidget {
   const RickAndMortyApp({super.key});
@@ -7,7 +10,12 @@ class RickAndMortyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: CharactersView(),
+      home: SelectionMenuView(),
+      routes: {
+        '/characters': (ctx) => CharactersView(),
+        '/episodes': (ctx) => EpisodesView(),
+        '/locations': (ctx) => LocationsView(),
+      },
     );
   }
 }
