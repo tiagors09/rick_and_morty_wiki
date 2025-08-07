39eedd7710ff167f0890571a186fab04e67ab98a
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2025-08-06 21:41:25 -0300

    added filters to get all characters

--- lib/data/services/character/get_all_characters_service.dart
+++ lib/data/services/character/get_all_characters_service.dart
@@ -3,16 +3,21 @@ import 'dart:convert';
 import 'package:rick_and_morty_wiki/data/services/service.dart';
 import 'package:rick_and_morty_wiki/domain/models/charater.dart';
 import 'package:rick_and_morty_wiki/exceptions/api_exception.dart';
+import 'package:rick_and_morty_wiki/utils/character_filters.dart';
 import 'package:rick_and_morty_wiki/utils/environment.dart';
 import 'package:http/http.dart' as http;
 
 class GetAllCharactersService implements Service<List<Character>> {
+  final CharacterFilters? filters;
+
+  GetAllCharactersService({this.filters});
+
   @override
   Future<List<Character>> execute() async {
     try {
       final response = await http.get(
         Uri.parse(
-          '${Environment.baseURL}/character/',
+          '${Environment.baseURL}/character/?name=${filters!.name ?? ''}&status=${filters!.status ?? ''}&species=${filters!.species ?? ''}&type=${filters?.type}&gender=${filters?.gender}',
         ),
       );
 
