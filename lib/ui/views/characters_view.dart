ceccecc97c76a553d0f487b4c2447e746aa9a5bf
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2025-08-07 01:30:25 -0300

    refactor character view

--- lib/ui/views/characters_view.dart
+++ lib/ui/views/characters_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:rick_and_morty_wiki/ui/viewmodels/characters_viewmodel.dart';
+import 'package:rick_and_morty_wiki/ui/widgets/character_filter_text_field.dart';
+import 'package:rick_and_morty_wiki/ui/widgets/character_list.dart';
 
 class CharactersView extends StatefulWidget {
   const CharactersView({super.key});
@@ -8,12 +11,48 @@ class CharactersView extends StatefulWidget {
 }
 
 class _CharactersViewState extends State<CharactersView> {
+  final viewModel = CharactersViewmodel();
+
+  @override
+  void initState() {
+    super.initState();
+    viewModel.fetchAllCharacters();
+    viewModel.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    viewModel.removeListener(() {});
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('olá'),
+      appBar: AppBar(
+        title: Text('Characters'),
+        bottom: CharactersFilterTextField(
+          onChange: viewModel.onFilterChange,
+        ),
       ),
+      body: viewModel.isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : viewModel.errorMessage != null
+          ? Center(
+              child: Text(
+                viewModel.errorMessage!,
+              ),
+            )
+          : RefreshIndicator(
+              onRefresh: () => viewModel.fetchAllCharacters(),
+              child: CharacterList(
+                characters: viewModel.characters,
+              ),
+            ),
     );
   }
 }
