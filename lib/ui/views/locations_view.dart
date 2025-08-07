d705339299aaf6509c698332187c18b4b9125ad2
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2025-08-07 15:32:01 -0300

    added episodes view

---
+++ lib/ui/views/locations_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LocationsView extends StatefulWidget {
+  const LocationsView({super.key});
+
+  @override
+  State<LocationsView> createState() => _LocationsViewState();
+}
+
+class _LocationsViewState extends State<LocationsView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Placeholder(),
+    );
+  }
+}
