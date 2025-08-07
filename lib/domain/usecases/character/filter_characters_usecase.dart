39a6849ff76a0a9802039af62ed9ad0ce9c33ec0
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2025-08-06 22:20:35 -0300

    removed filter char usecase

--- lib/domain/usecases/character/filter_characters_usecase.dart
+++
@@ -1,8 +0,0 @@
-import 'package:rick_and_morty_wiki/domain/usecases/usecase.dart';
-
-class FilterCharactersUsecase implements Usecase {
-  @override
-  void execute() {
-    // TODO: implement execute
-  }
-}
