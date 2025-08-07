6f73b13a6d4a315f0f1b6c1d8b371167fafd2099
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2025-08-07 16:11:55 -0300

    set landscape orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:rick_and_morty_wiki/rick_and_morty_app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ],
+  );
   runApp(RickAndMortyApp());
 }
