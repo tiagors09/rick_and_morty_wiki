a2823173a47ef62d1d554d487392767626f58064
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2025-08-07 16:43:33 -0300

    added text information formatted

--- lib/ui/views/character_view.dart
+++ lib/ui/views/character_view.dart
@@ -13,12 +13,22 @@ class CharacterView extends StatelessWidget {
         title: Text(c.name),
       ),
       body: Column(
+        spacing: 16,
         children: [
           Image.network(
             c.image,
             fit: BoxFit.fill,
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height / 1.5,
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(24, 0, 24, 0),
+            child: Text(
+              '${c.name} is a ${c.type.isNotEmpty ? "${c.type} " : ""}${c.species}, currently ${c.status}, of gender ${c.gender}, originally from ${c.origin.name}. Currently located in ${c.location.name}.',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
           ),
         ],
       ),
