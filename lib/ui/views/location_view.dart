65f6b5bdd0f90a6aea1ac1a517f1caabf37e1b42
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2025-08-07 14:36:19 -0300

    added location view

---
+++ lib/ui/views/location_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LocationView extends StatefulWidget {
+  const LocationView({super.key});
+
+  @override
+  State<LocationView> createState() => _LocationViewState();
+}
+
+class _LocationViewState extends State<LocationView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
