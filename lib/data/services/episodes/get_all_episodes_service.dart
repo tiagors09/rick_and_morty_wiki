4c8043c16264939f744dbc7d994c402d5d92bbad
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2025-08-08 00:24:32 -0300

    added request to get all episodes

--- lib/data/services/episodes/get_all_episodes_service.dart
+++ lib/data/services/episodes/get_all_episodes_service.dart
@@ -1,10 +1,40 @@
+import 'dart:convert';
+
 import 'package:rick_and_morty_wiki/data/services/service.dart';
 import 'package:rick_and_morty_wiki/domain/models/episode.dart';
+import 'package:rick_and_morty_wiki/exceptions/api_exception.dart';
+import 'package:rick_and_morty_wiki/utils/environment.dart';
+import 'package:rick_and_morty_wiki/utils/filters/episode_filters.dart';
+import 'package:http/http.dart' as http;
 
 class GetAllEpisodesService implements Service<List<Episode>> {
+  final EpisodeFilters? filters;
+  final int page;
+
+  GetAllEpisodesService({
+    this.filters,
+    this.page = 1,
+  });
+
   @override
-  Future<List<Episode>> execute() {
-    // TODO: implement execute
-    throw UnimplementedError();
+  Future<List<Episode>> execute() async {
+    try {
+      final response = await http.get(
+        Uri.parse(
+          '${Environment.baseURL}/episode/?',
+        ),
+      );
+
+      Map<String, dynamic> data = jsonDecode(response.body);
+      List<dynamic> results = data['results'];
+
+      List<Episode> episodes = results
+          .map((char) => Episode.fromJson(char as Map<String, dynamic>))
+          .toList();
+
+      return episodes;
+    } catch (_) {
+      throw ApiException('Falha ao obter personagens');
+    }
   }
 }
