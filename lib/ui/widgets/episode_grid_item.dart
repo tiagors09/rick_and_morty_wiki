f60d59231c85ff60ce8625bec01a028875a648ed
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2025-08-08 00:22:42 -0300

    added episode grid item

---
+++ lib/ui/widgets/episode_grid_item.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:rick_and_morty_wiki/domain/models/charater.dart';
+import 'package:rick_and_morty_wiki/domain/models/episode.dart';
+
+class EpisodeGridItem extends StatelessWidget {
+  final Episode e;
+  final void Function(BuildContext, Episode) onTap;
+
+  const EpisodeGridItem({super.key, required this.e, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GestureDetector(
+        onTap: () => onTap(context, e),
+        child: GridTile(
+          footer: GridTileBar(
+            title: Text(e.episode),
+            backgroundColor: Colors.black87,
+          ),
+          child: Hero(
+            tag: e.id,
+            child: FadeInImage.assetNetwork(
+              fit: BoxFit.cover,
+              placeholder: 'assets/images/placeholder.jpeg',
+              image: 'assets/images/placeholder.jpeg',
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
