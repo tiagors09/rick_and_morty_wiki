e139db957e6385fae76dbf41b772127bd8a28860
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2025-08-08 00:20:09 -0300

    added get multiple episodes base code

---
+++ lib/data/repositories/episodes/get_multiple_episodes_repository.dart
@@ -0,0 +1,15 @@
+import 'package:rick_and_morty_wiki/data/repositories/repository.dart';
+import 'package:rick_and_morty_wiki/data/services/service.dart';
+import 'package:rick_and_morty_wiki/domain/models/episode.dart';
+
+class GetMultipleEpisodesRepository implements Repository<List<Episode>> {
+  final Service service;
+
+  GetMultipleEpisodesRepository({required this.service});
+
+  @override
+  Future<List<Episode>> execute() {
+    // TODO: implement execute
+    throw UnimplementedError();
+  }
+}
