9db06e5395da84f10ebfc73bf73bf107fdfcb339
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2025-08-07 21:44:09 -0300

    added base code

---
+++ lib/data/services/location/get_all_locations_service.dart
@@ -0,0 +1,10 @@
+import 'package:rick_and_morty_wiki/data/services/service.dart';
+import 'package:rick_and_morty_wiki/domain/models/location.dart';
+
+class GetAllLocationsService implements Service<List<Location>> {
+  @override
+  Future<List<Location>> execute() {
+    // TODO: implement execute
+    throw UnimplementedError();
+  }
+}
---
+++ lib/data/services/location/get_multiple_locations.dart
@@ -0,0 +1,10 @@
+import 'package:rick_and_morty_wiki/data/services/service.dart';
+import 'package:rick_and_morty_wiki/domain/models/location.dart';
+
+class GetMultipleLocations implements Service<List<Location>> {
+  @override
+  Future<List<Location>> execute() {
+    // TODO: implement execute
+    throw UnimplementedError();
+  }
+}
